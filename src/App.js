8deb3071cef3500a8ba0a731d002990f9b6e8cdb
Duy Node <duynode@Duys-MBP.lan>
2023-10-27 23:03:33 +0700

    Thinking About State and Lifting State Up

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -21,7 +27,7 @@ function Logo() {
   return <h1>🏝️ Far Away 🧳</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
@@ -33,6 +39,8 @@ function Form() {
     const newItem = { description, quantity, packed: false, id: Date.now() };
 
     console.log("🚀 ~ file: App.js:33 ~ handleSubmit ~ newItem:", newItem);
+
+    onAddItems(newItem);
 
     setDescription("");
     setQuantity(1);
@@ -58,11 +66,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
